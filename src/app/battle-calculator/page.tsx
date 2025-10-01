d88b88acf14e6d14c9de55a735cd8e4eeee546de
DHCross <45954119+DHCross@users.noreply.github.com>
2025-10-01 18:23:44 -0500

    fix: clean battle calculator page imports

--- src/app/battle-calculator/page.tsx
+++ src/app/battle-calculator/page.tsx
@@ -3,18 +3,10 @@ import Link from 'next/link';
 import { resolveBackTargetFromSearchParams } from '../../utils/backNavigation';
 import { PagePropsWithSearchParams, resolvePageSearchParams } from '../../types/page';
 
-
 type PageProps = PagePropsWithSearchParams;
 
 export default async function BattleCalculatorPage({ searchParams }: PageProps) {
   const resolvedSearchParams = await resolvePageSearchParams(searchParams);
-
-type PageProps = {
-  searchParams?: Promise<Record<string, string | string[] | undefined>>;
-};
-
-export default async function BattleCalculatorPage({ searchParams }: PageProps) {
-  const resolvedSearchParams = searchParams ? await searchParams : undefined;
 
   const backTarget = resolveBackTargetFromSearchParams(resolvedSearchParams, 'gm-tools');
 
